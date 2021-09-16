b753c737a52ebd4807bc6d8526357d26819a70b8
Bashorun Olajide John <bashorunolajide@gmail.com>
2021-09-16 02:17:34 +0100

    Initial commit

---
+++ app/src/main/java/com/olajide/cryptocurrency/common/Constans.kt
@@ -0,0 +1,4 @@
+package com.olajide.cryptocurrency.common
+
+object Constans {
+}
---
+++ app/src/main/java/com/olajide/cryptocurrency/common/Resource.kt
@@ -0,0 +1,6 @@
+package com.olajide.cryptocurrency.common
+
+sealed class Resource<T>(val data: T?, val message: String?) {
+    class Success<T> (data: T) : Resource<T> (data, null)
+    class Failure<T> (message: String) : Resource<T>(null, message)
+}
